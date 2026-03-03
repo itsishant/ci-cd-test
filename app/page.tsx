e1f3c11cfe8f969672dfc09e9de50c7b1e1ad565
itsishant <modakmania@gmail.com>
2026-03-03 19:36:46 +0530

    add basic nextjs n page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-black min-h-screen flex flex-col items-center justify-center text-white">
+      <h1 className="text-4xl"> 
+        CI/CD Testing using ec2 instance and github actions
+      </h1>
+    </div>
+    );
+}
