e130919aaa0b692d412c355b8de5b7ba5148aecf
itsishant <modakmania@gmail.com>
2026-03-21 20:41:40 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,6 @@ export default function Home() {
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-center text-white">
       <h1 className="text-4xl"> 
-        CI/CD Testing using ec2 instance and github actions
-        
       </h1>
     </div>
     );
